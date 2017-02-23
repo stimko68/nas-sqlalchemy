b2240d65667615774b8c1fa1b0546aa087dd1162
Nick Simko <simko33f@gmail.com>
2017-02-22 21:23:16 -0700

    Added the ships model

--- sql_test.py
+++ sql_test.py
@@ -4,12 +4,18 @@ Simple script to play around with SQLAlchemy
 """
 from sqlalchemy import create_engine
 from sqlalchemy import Column
+from sqlalchemy import Float 
+from sqlalchemy import ForeignKey 
 from sqlalchemy import Integer
+from sqlalchemy import JSON 
 from sqlalchemy import String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 # Create the DB connection
-engine = create_engine('mysql+mysqlconnector://root@localhost/star_trek')
+engine = create_engine('mysql+mysqlconnector://root:password@localhost/star_trek')
+Session = sessionmaker(bind=engine)
+session = Session()
 
 # Create the declarative base class
 Base = declarative_base()
@@ -21,6 +27,19 @@ class Captain(Base):
     id = Column(Integer, primary_key=True)
     first_name = Column(String(50))
     last_name = Column(String(50))
+    meta = Column(JSON())
 
     def __repr__(self):
-        return "<Captain(name='{} {}')>".format(first_name, last_name)
+        return "<Captain(name='{} {}')>".format(self.first_name, self.last_name)
+
+class Ship(Base):
+    __tablename__ = 'ship'
+
+    id = Column(Integer, primary_key=True)
+    captain_fk = Column(Integer(), ForeignKey('captain.id'))
+    name = Column(String(50))
+    reg_number = Column(String(20))
+    max_warp_speed = Column(Float(), default=1)
+
+    def __repr__(self):
+        return "<Ship(name='{}')>".format(self.name)
